a35e9e229315bb0c5f7b33d06b7b73fb9681a70f
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-01-16 14:47:14 +0000

    Move to tawny-owl

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ncl.karyotype "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :plugins [[lein-swank "1.4.4"]]
+
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [uk.org.russet/clojure-owl "0.3-SNAPSHOT"]
+                 [uk.org.russet/tawny-owl "0.8-SNAPSHOT"]
                  ])
