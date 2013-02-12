ad7e4f3fb61dfa5156937dd603e202f17ceb650a
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-02-12 16:58:32 +0000

    1. Updated tawny-owl snapshot to 0.10

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :plugins [[lein-swank "1.4.4"]]
 
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [uk.org.russet/tawny-owl "0.9-SNAPSHOT"]
+                 [uk.org.russet/tawny-owl "0.10-SNAPSHOT"]
                  ]
 
   :main ncl.karyotype.core
