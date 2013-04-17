708d6e2fde5b94f792f9a3f6a497d0e3a8dc902b
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-04-17 17:23:47 +0100

    1. Updated

--- test/ncl/karyotype/test/core.clj
+++ test/ncl/karyotype/test/core.clj
@@ -1,6 +1,11 @@
 (ns ncl.karyotype.test.core
-  (:use [ncl.karyotype.core])
-  (:use [clojure.test]))
+  (:use [ncl.karyotype.core]
+        [clojure.test]
+        [tawny.owl])
+  (:require [ncl.karyotype [named :as n]]
+            [ncl.karyotype [events :as e]]
+            [ncl.karyotype [karyotype :as k]]
+            )
+   )
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+
