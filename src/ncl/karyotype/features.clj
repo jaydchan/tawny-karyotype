8bfc21a34a8051958f7893c88d28e78cfaa7883a
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-04-22 17:49:52 +0100

    1. Included inverse property 2. Added functions for isodicentric, dicentric, fragilesite, hsr, isoderivative, pseudo_dicentric, pseudo_isodicentric

--- src/ncl/karyotype/features.clj
+++ src/ncl/karyotype/features.clj
@@ -33,31 +33,63 @@
 (defclass Feature)
 
 ;; define object properties
-(defoproperty hasFeature
-  :range Feature
-  :domain k/Karyotype
-  )
+(as-inverse
+ (defoproperty hasFeature
+   :range Feature
+   :domain k/Karyotype
+   )
 
-
+ (defoproperty isFeatureOf
+   :range k/Karyotype
+   :domain Feature
+   )
+)
 
 ;; define all the structural features
 (as-disjoint-subclasses
  Feature
   (defclass DerivativeChromosome)
-
   (defn derivative [n chromosome & events]
     (exactly n hasFeature (owland DerivativeChromosome chromosome events)))
 
-  (defclass DicentricChromosome)
-  (defclass FragileSites)
-  (defclass HomogeneouslyStainingRegion)
-  (defclass IsoderiviativeChromosome)
+  ;; MUST be defined before dicentric as dicentric calls isodicentric function!
   (defclass Isochromosomes)
   (defclass IsodicentricChromosome)
+  (defn isodicentric [n band]
+    (exactly n hasFeature (owland IsodicentricChromosome (owlsome e/hasBreakPoint band))))
+
+  (defclass DicentricChromosome)
+  (defn dicentric [n band1 band2]
+    (if (= (str band1) (str band2))
+      (isodicentric n band1)
+      (exactly n hasFeature (owland DicentricChromosome (owlsome e/hasBreakPoint band1 band2)))))
+
+  (defclass FragileSite)
+  (defn fragilesite [n band]
+    (exactly n hasFeature (owland FragileSite (owlsome e/hasBreakPoint band))))
+
+  (defclass HomogeneouslyStainingRegion)
+  (defn hsr 
+    ([n band]
+       (exactly n hasFeature (owland HomogeneouslyStainingRegion (owlsome e/hasBreakPoint band))))
+    ([n band1 band2]
+       (exactly n hasFeature (owland HomogeneouslyStainingRegion (owlsome e/hasBreakPoint band1 band2)))))
+
+  (defclass IsoderivativeChromosome)
+  (defn isoderivative [n chromosome arm & events]
+    (exactly n hasFeature (owland IsoderivativeChromosome chromosome arm events)))
+
   (defclass MarkerChromosome)
   (defclass Neocentromere)
+  
   (defclass PseudodicentricChromosome)
+  (defn pseudo_dicentric [n band1 band2]
+    (exactly n hasFeature (owland PseudodicentricChromosome (owlsome e/hasBreakPoint band1 band2))))
+
   (defclass PseudoisodicentricChromosome)
+  (defn pseudo_isodicentric [n band]
+    (exactly n hasFeature (owland PseudoisodicentricChromosome (owlsome e/hasBreakPoint band))))
+
   (defclass RecombiantChromosome)
   (defclass RingChromosome)
   (defclass TelomericAssociations)
