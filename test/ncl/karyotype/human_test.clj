2520bf5cb103d581c271ee9ee3baf7304588c94b
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-06-14 16:04:48 +0100

    1. Added tests for pband? qband? ter? and cer? functions. 2. Also added commented out tests for ter?-new and cen?-new functions.

--- test/ncl/karyotype/human_test.clj
+++ test/ncl/karyotype/human_test.clj
@@ -35,3 +35,107 @@
 (deftest Basic
   (is (r/consistent?))
   (is (r/coherent?)))
+
+(deftest pBand?
+  (is (h/pband? "HumanChromosome1Bandp10"))
+  (is (h/pband? "HumanChromosome1Bandp11"))
+  (is (h/pband? "HumanChromosome1BandpTer"))
+  (is (h/pband? "HumanChromosome1Bandp"))
+
+  (is (not (h/pband? "HumanChromosome1Bandq10")))
+  (is (not (h/pband? "HumanChromosome1Bandq11")))
+  (is (not (h/pband? "HumanChromosome1BandqTer")))
+  (is (not (h/pband? "HumanChromosome1Bandq")))
+  (is (not (h/pband? "HumanChromosome1Band")))
+  (is (not (h/pband? "HumanChromosomeBand")))
+  (is (not (h/pband? "HumanChromosome"))))
+
+(deftest qBand?
+  (is (h/qband? "HumanChromosome1Bandq10"))
+  (is (h/qband? "HumanChromosome1Bandq11"))
+  (is (h/qband? "HumanChromosome1BandqTer"))
+  (is (h/qband? "HumanChromosome1Bandq"))
+
+  (is (not (h/qband? "HumanChromosome1Bandp10")))
+  (is (not (h/qband? "HumanChromosome1Bandp11")))
+  (is (not (h/qband? "HumanChromosome1BandpTer")))
+  (is (not (h/qband? "HumanChromosome1Bandp")))
+  (is (not (h/qband? "HumanChromosome1Band")))
+  (is (not (h/qband? "HumanChromosomeBand")))
+  (is (not (h/qband? "HumanChromosome"))))
+
+(deftest ter?
+  (is (h/ter? "HumanChromosome1BandqTer"))
+  (is (h/ter? "HumanChromosome1BandpTer"))
+
+  (is (not (h/ter? "HumanChromosome1Bandp10")))
+  (is (not (h/ter? "HumanChromosome1Bandp11")))
+  (is (not (h/ter? "HumanChromosome1Bandp")))
+  (is (not (h/ter? "HumanChromosome1Bandq10")))
+  (is (not (h/ter? "HumanChromosome1Bandq11")))
+  (is (not (h/ter? "HumanChromosome1Bandq")))
+  (is (not (h/ter? "HumanChromosome1Band")))
+  (is (not (h/ter? "HumanChromosomeBand")))
+  (is (not (h/ter? "HumanChromosome"))))
+
+(deftest cen?
+  (is (h/cen? "HumanChromosome1Bandq10"))
+  (is (h/cen? "HumanChromosome1Bandp10"))
+
+  (is (not (h/cen? "HumanChromosome1BandpTer")))
+  (is (not (h/cen? "HumanChromosome1Bandp11")))
+  (is (not (h/cen? "HumanChromosome1Bandp")))
+  (is (not (h/cen? "HumanChromosome1BandqTer")))
+  (is (not (h/cen? "HumanChromosome1Bandq11")))
+  (is (not (h/cen? "HumanChromosome1Bandq")))
+  (is (not (h/cen? "HumanChromosome1Band")))
+  (is (not (h/cen? "HumanChromosomeBand")))
+  (is (not (h/cen? "HumanChromosome"))))
+
+(deftest group-for-band
+  (is (str "HumanChromosome1Bandq")
+      (h/group-for-band "HumanChromosome1Band" "qTer"))
+  (is (str "HumanChromosome1Bandq")
+      (h/group-for-band "HumanChromosome1Band" "q11"))
+  (is (str "HumanChromosome1Bandq")
+      (h/group-for-band "HumanChromosome1Band" "q10"))
+  (is (str "HumanChromosome1Bandp")
+      (h/group-for-band "HumanChromosome1Band" "pTer"))
+  (is (str "HumanChromosome1Bandp")
+      (h/group-for-band "HumanChromosome1Band" "p11"))
+  (is (str "HumanChromosome1Bandp")
+      (h/group-for-band "HumanChromosome1Band" "p10")))
+
+;; ;; DO NOT REPLACE THE original ter? function! - Needed for string
+;; ;; compilation.
+;; (deftest ter?-new
+;;   (println (superclasses h/HumanChromosome1BandqTer))
+;;   (is (h/ter?-new h/HumanChromosome1BandqTer))
+;;   (is (h/ter?-new h/HumanChromosome1BandpTer))
+
+;;   (is (not (h/ter?-new h/HumanChromosome1Bandp10)))
+;;   (is (not (h/ter?-new h/HumanChromosome1Bandp11)))
+;;   (is (not (h/ter?-new h/HumanChromosome1Bandp)))
+;;   (is (not (h/ter?-new h/HumanChromosome1Bandq10)))
+;;   (is (not (h/ter?-new h/HumanChromosome1Bandq11)))
+;;   (is (not (h/ter?-new h/HumanChromosome1Bandq)))
+;;   (is (not (h/ter?-new h/HumanChromosome1Band)))
+;;   (is (not (h/ter?-new h/HumanChromosomeBand)))
+;;   (is (not (h/ter?-new h/HumanChromosome))))
+
+;; ;; DO NOT REPLACE THE original cen? function! - Needed for string
+;; ;; compilation.
+;; (deftest cen?-new
+;;   (println (superclasses h/HumanChromosome1BandqTer))
+;;   (is (h/cen?-new h/HumanChromosome1Bandq10))
+;;   (is (h/cen?-new h/HumanChromosome1Bandp10))
+
+;;   (is (not (h/cen?-new h/HumanChromosome1BandpTer)))
+;;   (is (not (h/cen?-new h/HumanChromosome1Bandp11)))
+;;   (is (not (h/cen?-new h/HumanChromosome1Bandp)))
+;;   (is (not (h/cen?-new h/HumanChromosome1BandqTer)))
+;;   (is (not (h/cen?-new h/HumanChromosome1Bandq11)))
+;;   (is (not (h/cen?-new h/HumanChromosome1Bandq)))
+;;   (is (not (h/cen?-new h/HumanChromosome1Band)))
+;;   (is (not (h/cen?-new h/HumanChromosomeBand)))
+;;   (is (not (h/cen?-new h/HumanChromosome))))
