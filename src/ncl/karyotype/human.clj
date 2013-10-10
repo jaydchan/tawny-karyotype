d0af25febb203889bfeeaafe695f2d750f3b2a44
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-10-10 15:03:13 +0100

    1. removed unnecessary require atoms.

--- src/ncl/karyotype/human.clj
+++ src/ncl/karyotype/human.clj
@@ -9,18 +9,15 @@
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see http://www.gnu.org/licenses/.
 
-
 (ns ncl.karyotype.human
   (:use [tawny.owl])
-  (:require [tawny
-             [reasoner :as r]
-             [read]]
+  (:require [tawny.read]
             [clojure.set]
             [ncl.karyotype [karyotype :as k]]))
 
