5a291e368ea5d1ecf968105e7537e6740e525093
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-10-10 15:04:55 +0100

    1. removed unnecessary require atoms.

--- src/ncl/karyotype/karyotype.clj
+++ src/ncl/karyotype/karyotype.clj
@@ -9,16 +9,14 @@
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see http://www.gnu.org/licenses/.
 
-
 (ns ncl.karyotype.karyotype
-  (:use [tawny.owl])
-  (:require [tawny [reasoner :as r]]))
+  (:use [tawny.owl]))
 
 (defontology karyotype
   :iri "http://ncl.ac.uk/karyotype/karyotype"
