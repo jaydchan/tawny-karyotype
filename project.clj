30a0939f1487bc25cbd1031ef2d1890d895db92b
Jennifer D Warrender <jennifer.warrender@newcastle.ac.uk>
2013-10-10 15:00:13 +0100

    1. Updated to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ncl.karyotype "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [uk.org.russet/tawny-owl "1.0-SNAPSHOT"]
                  ]
 
