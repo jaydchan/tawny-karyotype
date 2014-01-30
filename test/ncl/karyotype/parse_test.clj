752654d6f6ad39e41282af842f36cf1b6d70bb91
Jennifer Warrender <jaydchan@gmail.com>
2014-01-30 23:24:02 +0000

    1. Completed tests for addition-chromosome, addition-band, chrom-filter, band-filter and deletion-chromosome functions. 2. Started human-filter tests.

--- test/ncl/karyotype/parse_test.clj
+++ test/ncl/karyotype/parse_test.clj
@@ -18,6 +18,8 @@
 (ns ncl.karyotype.parse_test
   (:use [clojure.test])
   (:require
+   [ncl.karyotype.events :as e]
+   [ncl.karyotype.human :as h]
    [ncl.karyotype.parse :as p]
    [tawny.owl :as o]
    [tawny.reasoner :as r]))
@@ -37,3 +39,136 @@
 (deftest Basic
   (is (r/consistent?))
   (is (r/coherent?)))
+
+;; TODO Unknown chromosomes and bands
+
+;; (deftest Clean-Up)
+;; (deftest Get-Start)
+
+(deftest Addition-Chromosome
+  ;; valid inputs
+  (let [inputs [h/HumanChromosome1];; h/HumanAutosome h/HumanChromosome]
+        expected [["1" "+1"]];; ["?" "+?"] ["?" "+?"]]
+        actual (into [] (map #'ncl.karyotype.parse/addition-chromosome inputs))]
+
+    (doseq [i (range (count inputs))]
+      (let [string (get actual i)]
+        (is (every? string? string))
+        (is string (get expected i)))))
+
+  ;; invalid input
+  (is (thrown? AssertionError
+               "HumanChromosomeBand"
+               (#'ncl.karyotype.parse/addition-chromosome
+                h/HumanChromosomeBand))))
+
+(deftest Addition-Band
+  ;; valid inputs
+  (let [inputs [h/HumanChromosome1Bandp36.31 h/HumanChromosome1Bandp10
+                h/HumanChromosome1BandpTer] ;; h/HumanChromosome1Bandp
+                ;; h/HumanChromosome1Bandq h/HumanChromosome1Band
+                ;; h/HumanChromosomeBand]
+        expected [["1" "add(1)(p36.31)"] ["1" "add(1)(p10)"]
+                  ["1" "add(1)(pTer)"]]
+                  ;; ["1" "add(1)(p?)"] ["1" "add(1)(q?)"] ["1" "add(1)(?)"]
+                  ;; ["?" "add(?)(?)"]]
+        actual (into [] (map #'ncl.karyotype.parse/addition-band inputs))]
+
+    (doseq [i (range (count inputs))]
+      (let [string (get actual i)]
+        (is (every? string? string))
+        (is string (get expected i)))))
+
+  ;; invalid input
+  (is (thrown? AssertionError
+               "HumanChromosomeBand"
+               (#'ncl.karyotype.parse/addition-band h/HumanChromosome))))
+
+(deftest Chrom-Filter
+  ;; valid inputs
+  (let [inputs [(o/owl-and e/Addition h/HumanChromosome1)
+                (o/owl-and
+                 e/Addition
+                 (o/owl-some e/hasBreakPoint h/HumanChromosome1Bandp10))]
+        actual (into [] (map #'ncl.karyotype.parse/chrom-filter inputs))]
+
+     (is (h/chromosome? (first (get actual 0))))
+     (is (= h/HumanChromosome1 (first (get actual 0))))
+
+     (is (nil? (first (get actual 1))))))
+
+(deftest Band-Filter
+  ;; valid inputs
+  (let [inputs [(o/owl-and e/Addition h/HumanChromosome1)
+                (o/owl-and
+                 e/Addition
+                 (o/owl-some e/hasBreakPoint h/HumanChromosome1Bandp10))
+                (o/owl-and
+                 e/Deletion
+                 (o/owl-some e/hasBreakPoint
+                             h/HumanChromosome1Bandp10
+                             h/HumanChromosome1BandpTer))
+                (o/owl-and
+                 e/Deletion
+                 (o/owl-some e/hasBreakPoint
+                             h/HumanChromosome1Bandp10
+                             h/HumanChromosome1Bandq10))]
+        actual (into [] (map #'ncl.karyotype.parse/band-filter inputs))]
+
+    (is (nil? (first (get actual 0))))
+
+    (doseq [i (range 1 (count inputs))]
+      (let [output (get actual i)]
+        (doseq [band output]
+          (is (h/band? band)))))
+
+    (is (= 1 (count (get actual 1))))
+    (is (= 2 (count (get actual 2))))
+    (is (= 2 (count (get actual 3))))
+
+    (is (= h/HumanChromosome1Bandp10 (first (get actual 1))))
+    (let [expected [h/HumanChromosome1Bandp10 h/HumanChromosome1BandpTer]]
+          (is (or
+               (= (get actual 2) expected)
+               (= (get actual 2) (into [] (reverse expected))))))
+    (let [expected [h/HumanChromosome1Bandp10 h/HumanChromosome1Bandq10]]
+          (is (or
+               (= (get actual 3) expected)
+               (= (get actual 3) (into [] (reverse expected))))))))
+
+;; TODO
+(deftest Human-Filter
+  ;; valid inputs
+  (let [inputs [(o/owl-and e/Addition h/HumanChromosome1)
+                (o/owl-and
+                 e/Addition
+                 (o/owl-some e/hasBreakPoint h/HumanChromosome1Bandp10))]
+        actual (into [] (map #'ncl.karyotype.parse/human-filter inputs))]))
+
+;; (deftest Addition)
+
+(deftest Deletion-Chromosome
+  ;; valid inputs
+  (let [inputs [h/HumanChromosome1];; h/HumanAutosome h/HumanChromosome]
+        expected [["1" "-1"]];; ["?" "-?"] ["?" "-?"]]
+        actual (into [] (map #'ncl.karyotype.parse/deletion-chromosome inputs))]
+
+        (doseq [i (range (count inputs))]
+          (let [string (get actual i)]
+            (is (every? string? string))
+            (is string (get expected i)))))
+
+  ;; invalid input
+  (is (thrown? AssertionError
+               "HumanChromosomeBand"
+               (#'ncl.karyotype.parse/deletion-chromosome
+                h/HumanChromosomeBand))))
+
+;; (deftest Deletion-Band)
+;; (deftest Deletion-Band-Driver)
+;; (deftest Deletion)
+;; (deftest Get-Event-String)
+;; (deftest Get-Axiom_string)
+;; (deftest Chrom-Sort)
+;; (deftest Parse-Karyotype-Class)
+;; (deftest Create-Karyotype-String0)
