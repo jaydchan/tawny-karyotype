8a9d7bde63719809104496c4234aaf2b25d8a8e9
Jennifer Warrender <jennifer.warrender@newcastle.ncl.ac.uk>
2014-02-19 18:38:25 +0000

    1. Removed whitespace. 2. Removed uncessary documentation. 3. Commented out inconsistent entites for now.

--- test/ncl/karyotype/iscnexamples_test.clj
+++ test/ncl/karyotype/iscnexamples_test.clj
@@ -42,17 +42,14 @@
 ;; Ploidy Level
 ;; Haploid
 (deftest HaploidKaryotype
-  (is
-   (r/isuperclass? i/k26_X_+4_+6_+21 n/HaploidKaryotype))
-  )
+  (is (r/isuperclass? i/k26_X_+4_+6_+21 n/HaploidKaryotype)))
 
 ;; Diploid
 (deftest DiploidKaryotype
   (is (r/isuperclass? i/k44_Xc_-X n/DiploidKaryotype))
   (is (r/isuperclass? i/k45_X n/DiploidKaryotype))
   (is (r/isuperclass? i/k46_X_t!X_18!!p11.1_q11.1! n/DiploidKaryotype))
-  (is (r/isuperclass? i/k46_Xc_+X n/DiploidKaryotype))
-  )
+  (is (r/isuperclass? i/k46_Xc_+X n/DiploidKaryotype)))
 
 ;; Triploid
 (deftest TriploidKaryotype
@@ -61,8 +58,7 @@
        i/k69_XXY_del!7!!q22!_inv!7!!p13q22!_t!7_14!!p15_q11.1!
        n/TriploidKaryotype))
   (is (r/isuperclass? i/k70_XXX_+del!7!!p11.2! n/TriploidKaryotype))
-  (is (r/isuperclass? i/k71_XXX_+8_+10 n/TriploidKaryotype))
-  )
+  (is (r/isuperclass? i/k71_XXX_+8_+10 n/TriploidKaryotype)))
 
 ;; Tetraploid
 (deftest TetraploidKaryotype
@@ -71,11 +67,9 @@
   (is (r/isuperclass? i/k92_XXYY_del!7!!p11.2!_t!7_14!!p15_q11.1!
                       n/TetraploidKaryotype))
   (is (r/isuperclass? i/k92_XXYY_del!7!!p11.2!_del!7!!q22!_del!7!!q34!
-                      n/TetraploidKaryotype))
-  )
+                      n/TetraploidKaryotype)))
 
 ;; Gender
-;; TODO Test conditional karyotypes
 (deftest FemaleKaryotype
   (is (r/isuperclass?
        i/k49_X_inv!X!!p21q26!_+3_inv!3!!q21q26.2!_+7_+10_-20_del!20!!q11.2!_+21
@@ -107,10 +101,10 @@
   (is (r/isuperclass? i/k46_XX_inv!3!!q21q26.2! n/FemaleKaryotype))
   (is (r/isuperclass? i/k46_XX_qdp!1!!q23q32! n/FemaleKaryotype))
   (is (r/isuperclass? i/k46_XX_t!2_7_5!!p21_q22_q23! n/FemaleKaryotype))
-  (is
-   (r/isuperclass? i/k46_XX_t!3_9_22_21!!p13_q34_q11.2_q21! n/FemaleKaryotype))
-  (is
-   (r/isuperclass? i/k46_XX_t!5_14_9!!q13q23_q24q21_p12p23! n/FemaleKaryotype))
+  (is (r/isuperclass? i/k46_XX_t!3_9_22_21!!p13_q34_q11.2_q21!
+                      n/FemaleKaryotype))
+  (is (r/isuperclass? i/k46_XX_t!5_14_9!!q13q23_q24q21_p12p23!
+                      n/FemaleKaryotype))
   (is (r/isuperclass? i/k46_XX_t!9_22_17!!q34_q11.2_q22! n/FemaleKaryotype))
   (is (r/isuperclass? i/k46_XX_trp!1!!q21q32! n/FemaleKaryotype))
   (is (r/isuperclass? i/k47_X_t!X_13!!q27_q12!_inv!10!!p13q22!_+21
@@ -131,48 +125,41 @@
 
 (deftest MaleKaryotype
   (is (r/isuperclass? i/k45_Y n/MaleKaryotype))
-  (is (r/isuperclass? i/k46_XXYc_-X n/MaleKaryotype))
-  )
+  (is (r/isuperclass? i/k46_XXYc_-X n/MaleKaryotype)))
 
 ;; Numerical Abnormalities
-(deftest NumericalAbnormalKaryotype
-  )
 
-;; TOFIX
 ;; TODO - Need to decide on definition of abnormal - abnormal for
 ;; karyotype or abnormal for a human
 (deftest NumericalAbnormalKaryotypeAllosomalGain
   (is (r/isuperclass? i/k46_Xc_+X n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k46_YY n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k46_YY n/NumericalAbnormalKaryotypeAllosomalGain))
   (is (r/isuperclass? i/k47_XX_+X n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k47_XXX n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k47_XYY n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k47_YYY n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k48_XXXX n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k48_XYYY n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k48_YYYY n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k49_XXXXX n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k49_XYYYY n/NumericalAbnormalKaryotypeAllosomalGain))
-  ;; (is (r/isuperclass? i/k49_YYYYY n/NumericalAbnormalKaryotypeAllosomalGain))
-  )
+  (is (r/isuperclass? i/k47_XXX n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k47_XYY n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k47_YYY n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k48_XXXX n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k48_XYYY n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k48_YYYY n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k49_XXXXX n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k49_XYYYY n/NumericalAbnormalKaryotypeAllosomalGain))
+  (is (r/isuperclass? i/k49_YYYYY n/NumericalAbnormalKaryotypeAllosomalGain)))
 
-;; TOFIX
 (deftest NumericalAbnormalKaryotypeAllosomalLoss
   (is (r/isuperclass? i/k44_Xc_-X n/NumericalAbnormalKaryotypeAllosomalLoss))
   (is (r/isuperclass? i/k45_X_-X n/NumericalAbnormalKaryotypeAllosomalLoss))
   (is (r/isuperclass? i/k45_X_-Y n/NumericalAbnormalKaryotypeAllosomalLoss))
-  ;; (is (r/isuperclass? i/k45_Y n/NumericalAbnormalKaryotypeAllosomalLoss))
+  (is (r/isuperclass? i/k45_Y n/NumericalAbnormalKaryotypeAllosomalLoss))
   (is (r/isuperclass? i/k45_Y_-X n/NumericalAbnormalKaryotypeAllosomalLoss))
-  ;; (is (r/isuperclass? i/k46_YY n/NumericalAbnormalKaryotypeAllosomalLoss))
+  (is (r/isuperclass? i/k46_YY n/NumericalAbnormalKaryotypeAllosomalLoss))
   (is (r/isuperclass? i/k46_XXYc_-X n/NumericalAbnormalKaryotypeAllosomalLoss))
-  ;; (is (r/isuperclass? i/k47_YYY n/NumericalAbnormalKaryotypeAllosomalLoss))
-  ;; (is (r/isuperclass? i/k48_YYYY n/NumericalAbnormalKaryotypeAllosomalLoss))
-  ;; (is (r/isuperclass? i/k49_YYYYY n/NumericalAbnormalKaryotypeAllosomalLoss))
-  )
+  (is (r/isuperclass? i/k47_YYY n/NumericalAbnormalKaryotypeAllosomalLoss))
+  (is (r/isuperclass? i/k48_YYYY n/NumericalAbnormalKaryotypeAllosomalLoss))
+  (is (r/isuperclass? i/k49_YYYYY n/NumericalAbnormalKaryotypeAllosomalLoss)))
 
 (deftest NumericalAbnormalKaryotypeAutosomalGain
-  (is
-   (r/isuperclass? i/k26_X_+4_+6_+21 n/NumericalAbnormalKaryotypeAutosomalGain))
+  (is (r/isuperclass? i/k26_X_+4_+6_+21
+                      n/NumericalAbnormalKaryotypeAutosomalGain))
   (is (r/isuperclass? i/k47_XX_del!6!!q13q23!x2_+del!6!!q13q23!
                       n/NumericalAbnormalKaryotypeAutosomalGain))
   (is (r/isuperclass? i/k48_XX_+del!6!!q13q23!x2
@@ -181,19 +168,17 @@
                       n/NumericalAbnormalKaryotypeAutosomalGain))
   (is (r/isuperclass? i/k70_XXX_+del!7!!p11.2!
                       n/NumericalAbnormalKaryotypeAutosomalGain))
-  (is
-   (r/isuperclass? i/k71_XXX_+8_+10 n/NumericalAbnormalKaryotypeAutosomalGain))
-  (is (r/isuperclass? i/k89_XXYY_-1_-3_-5_+8_-21
+  (is (r/isuperclass? i/k71_XXX_+8_+10
                       n/NumericalAbnormalKaryotypeAutosomalGain))
-  )
+  (is (r/isuperclass? i/k89_XXYY_-1_-3_-5_+8_-21
+                      n/NumericalAbnormalKaryotypeAutosomalGain)))
 
-;; TOFIX
 (deftest NumericalAbnormalKaryotypeAutosomalLoss
   (is (r/isuperclass? i/k45_XX_-22 n/NumericalAbnormalKaryotypeAutosomalLoss))
-  (is
-   (r/isuperclass? i/k46_XX_+8_-21 n/NumericalAbnormalKaryotypeAutosomalLoss))
-  (is
-   (r/isuperclass? i/k46_XY_+21c_-21 n/NumericalAbnormalKaryotypeAutosomalLoss))
+  (is (r/isuperclass? i/k46_XX_+8_-21
+                      n/NumericalAbnormalKaryotypeAutosomalLoss))
+  (is (r/isuperclass? i/k46_XY_+21c_-21
+                      n/NumericalAbnormalKaryotypeAutosomalLoss))
   (is (r/isuperclass? i/k47_XY_-10_+fis!10!!p10!_+fis!10!!q10!
                       n/NumericalAbnormalKaryotypeAutosomalLoss))
   (is (r/isuperclass?
@@ -202,23 +187,22 @@
   (is (r/isuperclass? i/k89_XXYY_-1_-3_-5_+8_-21
                       n/NumericalAbnormalKaryotypeAutosomalLoss))
 
+  ;; TOFIX MISSING???
   ;; (is (r/isuperclass?
   ;;      i/k48_X_t!Y_12!!q11.2_p12!_del!6!!q11!_+8_t!9_22!!q34_q11.2!_+17_-21_+22
   ;;      n/NumericalAbnormalKaryotypeAutosomalLoss))
- ;; (is (r/isuperclass?
+  ;; (is (r/isuperclass?
   ;;      i/k50_XX_+1_del!1!!p13!_+dup!1!!q21q32!_+inv!1!!p31q41!_+8_r!10!!p12q25!_-21
   ;;      n/NumericalAbnormalKaryotypeAutosomalLoss))
-  )
+)
 
 ;; Structural Abnormalities
 (deftest StructuralAbnormalKaryotypeAddition
   (is (r/isuperclass? i/k46_XX_add!19!!p13.3!
                       n/StructuralAbnormalKaryotypeAddition))
-  (is
-   (r/isuperclass? i/k46_XY_add!12!!q13! n/StructuralAbnormalKaryotypeAddition))
-  )
+  (is (r/isuperclass? i/k46_XY_add!12!!q13!
+                      n/StructuralAbnormalKaryotypeAddition)))
 
-;; TOFIX
 (deftest StructuralAbnormalKaryotypeDeletion
   (is (r/isuperclass?
        i/k49_X_inv!X!!p21q26!_+3_inv!3!!q21q26.2!_+7_+10_-20_del!20!!q11.2!_+21
@@ -252,6 +236,7 @@
   (is (r/isuperclass? i/k92_XXYY_del!7!!p11.2!_t!7_14!!p15_q11.1!
                       n/StructuralAbnormalKaryotypeDeletion))
 
+  ;; TOFIX MISSING???
   ;; (is (r/isuperclass?
   ;;      i/k48_X_t!Y_12!!q11.2_p12!_del!6!!q11!_+8_t!9_22!!q34_q11.2!_+17_-21_+22
   ;;      n/StructuralAbnormalKaryotypeDeletion))
@@ -261,13 +246,11 @@
   (is (r/isuperclass? i/k46_XX_dup!1!!q22q25!
                       n/StructuralAbnormalKaryotypeDuplication))
   (is (r/isuperclass? i/k46_XY_dup!1!!q25q22!
-                      n/StructuralAbnormalKaryotypeDuplication))
-  )
+                      n/StructuralAbnormalKaryotypeDuplication)))
 
 (deftest StructuralAbnormalKaryotypeFission
   (is (r/isuperclass? i/k47_XY_-10_+fis!10!!p10!_+fis!10!!q10!
-                      n/StructuralAbnormalKaryotypeFission))
-  )
+                      n/StructuralAbnormalKaryotypeFission)))
 
 (deftest StructuralAbnormalKaryotypeInsertion
   (is (r/isuperclass? i/k46_X_ins!5_X!!p14_q21q25!
@@ -291,8 +274,7 @@
   (is (r/isuperclass? i/k46_XY_ins!5_2!!p14_q22q32!
                       n/StructuralAbnormalKaryotypeInsertion))
   (is (r/isuperclass? i/k46_XY_ins!5_2!!p14_q32q22!
-                      n/StructuralAbnormalKaryotypeInsertion))
-  )
+                      n/StructuralAbnormalKaryotypeInsertion)))
 
 (deftest StructuralAbnormalKaryotypeInversion
   (is (r/isuperclass?
@@ -313,6 +295,7 @@
   (is (r/isuperclass? i/k47_Y_t!X_13!!q27_q12!_inv!10!!p13q22!_+21
                       n/StructuralAbnormalKaryotypeInversion))
 
+  ;; TOFIX MISSING???
   ;; (is (r/isuperclass? i/k47_X_t!Y_13!!q27_q12!_inv!10!!p13q22!_+21
   ;;                     n/StructuralAbnormalKaryotypeInversion))
   ;; (is (r/isuperclass?
@@ -322,8 +305,7 @@
 
 (deftest StructuralAbnormalKaryotypeQuadruplication
   (is (r/isuperclass? i/k46_XX_qdp!1!!q23q32!
-                      n/StructuralAbnormalKaryotypeQuadruplication))
-  )
+                      n/StructuralAbnormalKaryotypeQuadruplication)))
 
 (deftest StructuralAbnormalKaryotypeTranslocation
 
@@ -387,150 +369,100 @@
   (is (r/isuperclass? i/k46_XX_invtrp!1!!q32q21!
                       n/StructuralAbnormalKaryotypeTriplication))
   (is (r/isuperclass? i/k46_XX_trp!1!!q21q32!
-                      n/StructuralAbnormalKaryotypeTriplication))
-  )
+                      n/StructuralAbnormalKaryotypeTriplication)))
 
 ;; Named Karyotypes - Allosomal Abnormalities
-;; TOFIX
+
 (deftest TurnerSyndrome
+  ;; TOFIX
   ;; (is (r/isuperclass? i/k45_X n/TurnerSyndrome))
-  )
+)
 
-(deftest KlinefelterSyndromeMostCommonVariation
-  ;; TOFIX - should be TRUE
-  ;; (is (r/isuperclass? i/k47_XXY n/KlinefelterSyndromeMostCommonVariation))
+(deftest KlinefelterSyndrome
+  ;; TOFIX
+  ;; (is (r/isuperclass? i/k47_XXY n/KlinefelterSyndrome))
 
-  (is (not
-       (r/isuperclass? i/k48_XXYY n/KlinefelterSyndromeMostCommonVariation)))
-  (is (not
-       (r/isuperclass? i/k48_XXYc_+X n/KlinefelterSyndromeMostCommonVariation)))
-  (is
-   (not
-    (r/isuperclass? i/k48_XY_+X_+Y n/KlinefelterSyndromeMostCommonVariation)))
-  (is (not
-       (r/isuperclass? i/k49_XXYYY n/KlinefelterSyndromeMostCommonVariation)))
-  )
+  (is (not (r/isuperclass? i/k48_XXYY n/KlinefelterSyndrome)))
+  (is (not (r/isuperclass? i/k48_XXYc_+X n/KlinefelterSyndrome)))
+  (is (not (r/isuperclass? i/k48_XY_+X_+Y n/KlinefelterSyndrome)))
+  (is (not (r/isuperclass? i/k49_XXYYY n/KlinefelterSyndrome))))
 
-(deftest KlinefelterSyndromeAllVariations
-  ;; (is (r/isuperclass? i/k48_XXXY n/KlinefelterSyndromeAllVariations))
-  ;; (is (r/isuperclass? i/k49_XXXXY n/KlinefelterSyndromeAllVariations))
-  ;; (is (r/isuperclass? i/k49_XXXYY n/KlinefelterSyndromeAllVariations))
-  ;; (is (r/isuperclass? i/k48_XXYY n/KlinefelterSyndromeAllVariations))
-  (is (r/isuperclass? i/k48_XXYc_+X n/KlinefelterSyndromeAllVariations))
-  (is (r/isuperclass? i/k48_XY_+X_+Y n/KlinefelterSyndromeAllVariations))
-  ;; (is (r/isuperclass? i/k49_XXYYY n/KlinefelterSyndromeAllVariations))
-  )
+(deftest MaleWithExtraX
+  ;; TOFIX
+  ;; (is (r/isuperclass? i/k48_XXXY n/MaleWithExtraX))
+  ;; (is (r/isuperclass? i/k49_XXXXY n/MaleWithExtraX))
+  ;; (is (r/isuperclass? i/k49_XXXYY n/MaleWithExtraX))
+  ;; (is (r/isuperclass? i/k48_XXYY n/MaleWithExtraX))
+  ;; (is (r/isuperclass? i/k48_XXYc_+X n/MaleWithExtraX))
+  ;; (is (r/isuperclass? i/k48_XY_+X_+Y n/MaleWithExtraX))
+  ;; (is (r/isuperclass? i/k49_XXYYY n/MaleWithExtraX))
+)
 
-;; NONE DEFINED
-(deftest Lethal
-  )
+;; (deftest MaleWithExtraY)
+;; (TripleXSyndrome)
+;; (deftest FemaleWithExtraX)
+;; (deftest Lethal)
 
 ;; TOFIX
 ;; Named Karyotypes - Autsomal Abnormalities
-(deftest Warkany2Syndrome
-  (is (r/isuperclass? i/k46_XX_+8_-21 n/Warkany2Syndrome))
+(deftest WarkanySyndrome2
+  ;; TOFIX
+  ;; (is (r/isuperclass? i/k46_XX_+8_-21 n/WarkanySyndrome2))
 
+  ;; TOFIX MISSING???
   ;; (is (r/isuperclass?
   ;;      i/k48_X_t!Y_12!!q11.2_p12!_del!6!!q11!_+8_t!9_22!!q34_q11.2!_+17_-21_+22
-  ;;      n/Warkany2Syndrome))
+  ;;      n/WarkanySyndrome2))
   ;; (is (r/isuperclass?
   ;;      i/k50_XX_+1_del!1!!p13!_+dup!1!!q21q32!_+inv!1!!p31q41!_+8_r!10!!p12q25!_-21
-  ;;      n/Warkany2Syndrome))
+  ;;      n/WarkanySyndrome2))
 )
 
-;; NONE DEFINED
-(deftest Trisomy9
-  )
+;; (deftest Trisomy9)
 
 (deftest PatauSyndrome
-  (is (r/isuperclass? i/k48_XX_+13_+21 n/PatauSyndrome))
-  )
+  ;; TOFIX
+  ;; (is (r/isuperclass? i/k48_XX_+13_+21 n/PatauSyndrome))
+)
 
-;; NONE DEFINED
-(deftest Trisomy16
-  )
-
-;; NONE DEFINED
-(deftest EdwardsSyndrome
-  )
+;; (deftest Trisomy16)
+;; (deftest EdwardsSyndrome)
 
 (deftest DownSyndrome
-  (is (r/isuperclass?
-       i/k49_X_inv!X!!p21q26!_+3_inv!3!!q21q26.2!_+7_+10_-20_del!20!!q11.2!_+21
-       n/DownSyndrome))
-  (is (r/isuperclass? i/k46_Xc_+21 n/DownSyndrome))
-  (is
-   (r/isuperclass? i/k47_X_t!X_13!!q27_q12!_inv!10!!p13q22!_+21 n/DownSyndrome))
-  (is (r/isuperclass? i/k47_XX_+21 n/DownSyndrome))
-  (is
-   (r/isuperclass? i/k47_Y_t!X_13!!q27_q12!_inv!10!!p13q22!_+21 n/DownSyndrome))
-  (is (r/isuperclass? i/k48_XX_+13_+21 n/DownSyndrome))
+  ;; TOFIX
+  ;; (is (r/isuperclass?
+  ;;      i/k49_X_inv!X!!p21q26!_+3_inv!3!!q21q26.2!_+7_+10_-20_del!20!!q11.2!_+21
+  ;;      n/DownSyndrome))
+  ;; (is (r/isuperclass? i/k46_Xc_+21 n/DownSyndrome))
+  ;; (is (r/isuperclass? i/k47_X_t!X_13!!q27_q12!_inv!10!!p13q22!_+21
+  ;;                     n/DownSyndrome))
+  ;; (is (r/isuperclass? i/k47_XX_+21 n/DownSyndrome))
+  ;; (is (r/isuperclass? i/k47_Y_t!X_13!!q27_q12!_inv!10!!p13q22!_+21
+  ;;                     n/DownSyndrome))
+  ;; (is (r/isuperclass? i/k48_XX_+13_+21 n/DownSyndrome))
 
   ;; TODO - NOT TRUE - THE DEFINITION FOR DOWN SYNDROMES UPDATING
-  ;; (is (not (r/isuperclass? i/k48_XY_+21c_+21 n/DownSyndrome)))
+  (is (not (r/isuperclass? i/k48_XY_+21c_+21 n/DownSyndrome)))
   )
 
-;; TOFIX
-(deftest Trisomy22
-  ;; (is (r/isuperclass? i/k48_X_t!Y_12!!q11.2_p12!_del!6!!q11!_+8_t!9_22!!q34_q11.2!_+17_-21_+22 n/Trisomy22))
-  )
+(deftest CatEyeSyndrome
+  ;; TOFIX MISSING???
+  ;; (is (r/isuperclass?
+  ;;      i/k48_X_t!Y_12!!q11.2_p12!_del!6!!q11!_+8_t!9_22!!q34_q11.2!_+17_-21_+22
+  ;;      n/CatEyeSyndrome))
+)
 
 ;; Named Karyotypes - Structural Abnormalities
 
-;; TODO
-(deftest DeletionSyndrome1p36
-  )
-
-;; TODO
-(deftest WolfHirschhornSyndrome
-  )
-
-;; TODO
-(deftest CriDuChat
-  )
-
-;; TODO
-(deftest DeletionSyndrome5q
-  )
-
-;; TODO
-(deftest WilliamsSyndrome
-  )
-
-;; TODO
-(deftest JacobsenSyndrome
-  )
-
-;; TODO
-(deftest AngelmanSyndrome
-  )
-
-;; TODO
-(deftest PraderWilliSyndrome
-  )
-
-;; TODO
-(deftest MillerDiekerSyndrome
-  )
-
-;; TODO
-(deftest SmithMagenisSyndrome
-  )
-
-;; TODO
-(deftest DeletionSyndrome18q
-  )
-
-;; TODO
-(deftest DiGeorgeSyndrome
-  )
-
-;; TODO
-(deftest CatEyeSyndrome
-  )
-
-;; TODO Probe classes
-;; (is (not
-;;      (with-probe-class
-;;        [joined (owl-class :subclass i/k46_XY i/k45_Y)]
-;;        (r/consistent? joined))))
+;; (deftest DeletionSyndrome1p36)
+;; (deftest WolfHirschhornSyndrome)
+;; (deftest CriDuChat)
+;; (deftest Chromosome51DeletionSyndrome)
+;; (deftest WilliamsSyndrome)
+;; (deftest JacobsenSyndrome)
+;; (deftest AngelmanSyndrome)
+;; (deftest PraderWilliSyndrome)
+;; (deftest MillerDiekerSyndrome)
+;; (deftest SmithMagenisSyndrome)
+;; (deftest Chromosome18qDeletionSyndrome)
+;; (deftest DiGeorgeSyndrome)
