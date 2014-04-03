78893d956884bf153284193085842914d3df3392
Jennifer Warrender <jaydchan@gmail.com>
2014-04-03 23:54:24 +0100

    removed unecessary file

--- test/ncl/karyotype/test/core.clj
+++
@@ -1,9 +0,0 @@
-(ns ncl.karyotype.test.core
-  (:use [ncl.karyotype.core]
-        [clojure.test]
-        [tawny.owl])
-  (:require [ncl.karyotype [named :as n]]
-            [ncl.karyotype [events :as e]]
-            [ncl.karyotype [karyotype :as k]]
-            )
-  )
